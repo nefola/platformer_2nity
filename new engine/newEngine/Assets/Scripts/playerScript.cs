9f9ff657d244d0ddae7b5af1b688eed8b548abae
nefolaC4 <kidanumber1@gmail.com>
2018-02-22 14:28:24 -0500

    2/22/18
    Baced on [Unity] Creating a 2D Platformer (E02. collision detection)
    https://www.youtube.com/watch?v=OBtaLCmJexk
    
    right rays are kinda broken
    obstacle is invisable

--- new engine/newEngine/Assets/Scripts/playerScript.cs
+++ new engine/newEngine/Assets/Scripts/playerScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent (typeof (controller2D))]
 public class playerScript : MonoBehaviour {
 
+    float moveSpeed = 6;
+    float gravity = -20;
+    Vector3 velocity;
+
     controller2D controller;
 	
 	void Start () {
@@ -12,4 +16,12 @@ public class playerScript : MonoBehaviour {
         controller = GetComponent<controller2D>();
 	}
 
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
